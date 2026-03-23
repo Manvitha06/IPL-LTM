fe1f2f87a63a0f56e1eed60d985b18d0603946a7
Manvitha06 <dummy@gmail.com>
2026-03-23 13:06:34 +0000

    CompleteWorkingCode

--- client/src/app/ipl/components/teamedit/teamedit.component.ts
+++ client/src/app/ipl/components/teamedit/teamedit.component.ts
@@ -1,11 +1,11 @@
-
+ 
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IplService } from '../../services/ipl.service';
 import { Team } from '../../types/Team';
 import { ActivatedRoute, Router } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-teamedit',
   templateUrl: './teamedit.component.html',
@@ -18,13 +18,13 @@ export class TeamEditComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
   team: Team | null = null;
   teamId!: number;
-
+ 
   constructor(
     private formBuilder: FormBuilder,
     private iplService: IplService,
     private route: ActivatedRoute,
   ) {}
-
+ 
   ngOnInit(): void {
     this.teamForm = this.formBuilder.group({
         teamName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ]+$/)]], // No special characters
@@ -41,7 +41,7 @@ export class TeamEditComponent implements OnInit {
         this.loadTeamDetails(this.teamId);
     });
   }
-
+ 
   loadTeamDetails(teamId: number): void {
     this.iplService.getTeamById(teamId).subscribe({
         next: (response) => {
@@ -58,7 +58,7 @@ export class TeamEditComponent implements OnInit {
         }
     });
 }
-
+ 
   onSubmit(): void {
     if (this.teamForm.valid) {
         const updatedTeam: Team = {
@@ -85,7 +85,7 @@ export class TeamEditComponent implements OnInit {
         });
     } 
   }
-
+ 
   private handleError(error: HttpErrorResponse): void {
     if (error.error instanceof ErrorEvent) {
       this.errorMessage = `Client-side error: ${error.error.message}`;
--- server/src/main/java/com/edutech/progressive/controller/TeamController.java
+++ server/src/main/java/com/edutech/progressive/controller/TeamController.java
@@ -5,7 +5,7 @@ import com.edutech.progressive.exception.TeamDoesNotExistException;
 import com.edutech.progressive.service.TeamService;
 import com.edutech.progressive.service.impl.TeamServiceImplArraylist;
 import com.edutech.progressive.service.impl.TeamServiceImplJpa;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
 import java.sql.SQLException;
 import java.util.List;
- 
- 
+
 @RestController
 @RequestMapping("/team")
 public class TeamController {
@@ -66,7 +64,6 @@ public class TeamController {
             } catch (TeamDoesNotExistException | TeamAlreadyExistsException e) {
                 return ResponseEntity.status(400).body(e.getMessage());
             }
-       
         return ResponseEntity.ok().build(); // 200
     }
     @DeleteMapping("/{teamId}")
@@ -74,18 +71,15 @@ public class TeamController {
         teamServiceJpa.deleteTeam(teamId);
         return ResponseEntity.noContent().build(); // 204
     }
- 
- 
+
     @GetMapping("/fromArrayList")
     public ResponseEntity<List<Team>> getAllTeamsFromArrayList() throws SQLException {
         return ResponseEntity.ok(teamServiceArraylist.getAllTeams());
     }
- 
     @GetMapping("/fromArrayList/sorted")
     public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() throws SQLException {
         return ResponseEntity.ok(teamServiceArraylist.getAllTeamsSortedByName());
     }
- 
     @PostMapping("/toArrayList")
     public ResponseEntity<Integer> addTeamToArrayList(@RequestBody Team team) throws SQLException {
         int size = teamServiceArraylist.addTeam(team);
